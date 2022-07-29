ab45e8043ad88ab9ba51fd6c90741afe674626ba
tarte-351123 <k19021kk@aitech.ac.jp>
2022-07-29 14:45:33 +0900

    first

--- ble関連プログラム/mongodb-with-fastapi-master/POST.py
+++ ble関連プログラム/mongodb-with-fastapi-master/POST.py
@@ -2,47 +2,28 @@ import requests
 import pprint
 import datetime
 from datetime import datetime
+import csv
 
-POST_URL = "http://localhost:8000/cluster/"
+POST_URL = "http://localhost:8000/result_phone/"
+filename = '../csv/result_phone.csv'
 
+pattern = []
+# 配列のコピー
+with open(filename, encoding='utf8', newline='') as f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        pattern.append(row)
 
-# リクエストボディを定義する
-request_body = {
-    "dateType": "平日",
-    "normalDistribution": {
-        "mean": "",
-        "std": "",
-        "range": [{
-        "from_": "",
-        "to_": ""
-        },
-        {
-        "from": "",
-        "to": ""
-        }, 
-        {
-        "from": "",
-        "to": ""
-        }, ]
-    },
-    "model": [{
-        "object": "スマホと財布",
-        "percentage": "%"
-        },
-        {
-        "object": "財布とカバン",
-        "percentage": "%" 
-        },
-        {
-        "object": "カバンとスマホ",
-        "percentage": "%"
-        },
-    ]
-}
+for i in pattern:
+    print(i)
+    # リクエストボディを定義する
+    request_body = {
+        
+    }
 
 
 # POSTリクエストを、リクエストボディ付きで送信する
-response = requests.post(POST_URL, json=request_body)
+# response = requests.post(POST_URL, json=request_body)
 
 # レスポンスボディを出力する
 pprint.pprint(response.json())
