e371f6f3f2a8753f6eec694652414a624b7a4ca2
tarte-351123 <k19021kk@aitech.ac.jp>
2022-07-30 15:13:59 +0900

    update presume.py

--- ble関連プログラム/mongodb-with-fastapi-master/presume.py
+++ ble関連プログラム/mongodb-with-fastapi-master/presume.py
@@ -12,12 +12,15 @@ sample_time_data = "19:20"
 sample_time = int(sample_time_data[0:2])*60+int(sample_time_data[3:5])
 print(sample_time_data)
 data_list = []
+
+cluster = []
 for i in data:
     list = i["dateType"],int(i["normalDistribution"]["range"][2]["from_"][0:2])*60+int(i["normalDistribution"]["range"][2]["from_"][3:5]),int(i["normalDistribution"]["range"][2]["to_"][0:2])*60+int(i["normalDistribution"]["range"][2]["to_"][3:5])
     if sample_data_type == list[0] and list[1]<=sample_time and sample_time<=list[2]:
         print("cluster!")
-        print(i["dateType"])
-        print(i["normalDistribution"]["range"][2])
-        for i in i["model"]:
-            print(i)
+        cluster.append(i)
+        
+for i  in cluster:
+    print(i)
+
 
